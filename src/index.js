99bb785bc94afdb7f2a67c58a82238fa0f95fb35
Vishnu Goel <you@example.com>
2021-10-12 16:20:27 +0530

    react deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Route from './RouteComponent';
+import RefDemo from './RefDemoComponent';
+import AutoRefDemo from './AutoRefDemo';
 
 
-ReactDom.render(<Route/>,document.querySelector('#root'))
+ReactDom.render(<AutoRefDemo/>,document.querySelector('#root'))
